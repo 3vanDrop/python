d90a58cd9e333a2d4c2b545ceca7ed82dfc3a366
3vanDrop <evan.itf@gmail.com>
2019-01-13 15:27:54 -0600

    Calculate Weekday - Date Class + Weekday Class

--- WeekdayCalc/calc_weekday.py
+++ WeekdayCalc/calc_weekday.py
@@ -6,15 +6,55 @@
 @Author:
         -- 3van --
 """
-
-
 from datetime import datetime
 
-class Day(object):
+class Date(object):
+    """ -- Date Class - Solution -- """
 
     def __init__(self, *date):
         if len(date) != 3:
             raise ValueError('Expecting Day, Month & Year  as argument')
+        self.date = date
+
+    def __call__(self):
+        return self.year_day_count() + self.day_count()
+    
+    def weekday(self):
+        return (self.year_day_count() + self.day_count())%7
+
+    def year_day_count(self):
+        year = self.date[2]
+        base = 365*(year-1)
+        return base + int(self.leap_years())
+
+    def day_count(self):
+        day, month, year = self.date
+        days_per_month = (
+            31, 28 + int(self.is_leap(year)),
+            31, 30, 31, 30, 31, 31, 30, 31, 30, 31        
+        )
+        count = 0
+        for m in [i for i in range(month-1)]:
+            count += days_per_month[m]
+        return count + day-1
+
+    def leap_years(self):
+        year = self.date[2]
+        base_leap = float( year/4 )
+        div_by_100 = float( year/100 )
+        div_by_400 = float( year/400 )
+    
+        return base_leap - (div_by_100-div_by_400)
+
+    def is_leap(self, year):
+        if not (year%4)==0:
+            return False
+        if (year%100)==0 and not (year%400)==0:
+            return False
+        return True
+
+class Weekday:
+    def __init__(self, *date):
         self.days = {
             0:'Monday',
             1:'Tuesday',
@@ -24,10 +64,12 @@ class Day(object):
             5:'Saturday',
             6:'Sunday'
         }
-        self.date = datetime(*reversed(date))
+        self.date = Date(*date)
 
-    def weekday(self, *date):
+    def weekday(self):
         return self.days[ self.date.weekday() ]
 
 if __name__ == '__main__':
-    day = Day(12, 1, 2019)
+    date = datetime.today()
+    today = Weekday(date.day, date.month, date.year)
+    print('Today is', today.weekday())
